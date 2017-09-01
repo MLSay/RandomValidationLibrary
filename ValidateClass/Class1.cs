16958e095406b9448e64e661f388d996fdb5d0dd
Heav <heavchour@gmail.com>
2017-09-01 10:00:02 +1000

    Add project files.

---
+++ ValidateClass/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ValidateClass
+{
+    public class clsValidation
+    {
+        public static bool ValidateTextBox(TextBox txt)
+        {
+            if (string.IsNullOrEmpty(txt.Text))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
